97e0302167b87fd78c5f57b8fa8107053ebeef2f
Farid Indrawan <Zayidx>
2025-10-08 11:08:54 +0700

    Update Controller Auth

--- app/Http/Controllers/AuthController.php
+++ app/Http/Controllers/AuthController.php
@@ -8,39 +8,91 @@ use Illuminate\Support\Facades\Validator;
 
 class AuthController extends Controller
 {
-    
-    public function login(Request $request){
+
+    public function login(Request $request)
+    {
         $userData = Validator::make(
-        $request->all(),
-        [
-            'email' => 'required|email',
-            'password' => 'required'
-        ]);
+            $request->all(),
+            [
+                'email' => 'required|email',
+                'password' => 'required',
+            ]
+        );
 
         if ($userData->fails()) {
             return response()->json($userData->errors(), 422);
-
         }
 
         $credentials = $request->only('email', 'password');
-        if (!Auth()->attempt($credentials)) {
+
+        if (!Auth::attempt($credentials)) {
             return response()->json([
                 'success' => false,
-                'message' => 'Email atau Password Anda Salah'
+                'message' => 'Email atau Password Anda Salah',
             ], 401);
         }
+
         $user = Auth::user();
         $token = $user->createToken('appKu')->accessToken;
+
         return response()->json([
             'success' => true,
             'user' => $user,
-            'token' => $token
+            'token' => $token,
         ], 200);
     }
-    public function check(Request $request){
 
+    public function check(Request $request)
+    {
+        if (!Auth::guard('api')->check()) {
+            return response()->json([
+                'success' => false,
+                'message' => 'Unauthenticated',
+            ], 401);
+        }
+
+        $user = Auth::guard('api')->user();
+
+        return response()->json([
+            'success' => true,
+            'data' => [
+                'user' => $user,
+            ],
+        ], 200);
     }
-    public function logout(Request $request){
 
+    public function logout(Request $request)
+    {
+        $auth = auth('api');
+        try {
+            if ($auth->check()) {
+                $auth->user()->token()->revoke();
+                $auth->user()->token()->delete();
+                $pesan = [
+                    'status' => true,
+                    'pesan' => 'Logout Berhasil',
+                ];
+                $code = 200;
+            } else {
+                $pesan = [
+                    'status' => false,
+                    'pesan' => 'Sedang tidak login',
+                ];
+                $code = 401;
+
+            }
+
+
+              
+           
+        }
+        catch (\Exception $e) {
+            $pesan = [
+                'status' => false,
+                'pesan' => 'Gagal Logout',
+            ];
+            $code = 500;
+        }
+        return response()->json($pesan, $code);
     }
 }
--- routes/web.php
+++ routes/web.php
@@ -5,3 +5,9 @@ use Illuminate\Support\Facades\Route;
 Route::get('/', function () {
     return view('welcome');
 });
+
+Route::prefix('/auth')->group(function () {
+    Route::post('/login', [App\Http\Controllers\AuthController::class, 'login'])->name('auth.api');
+    Route::get('/profile', [App\Http\Controllers\AuthController::class, 'check'])->name('auth.api.check');
+    Route::post('/logout', [App\Http\Controllers\AuthController::class, 'logout'])->name('auth.api.logout');
+});
